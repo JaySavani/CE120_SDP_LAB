09f323ad0b83e8e4c865f02fbb45cb14f564c2f6
JaySavani <86875898+JaySavani@users.noreply.github.com>
2022-10-05 23:21:52 +0530

    Add files via upload

---
+++ Lab-11/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:lab_11/pages/home.dart';
+import 'package:lab_11/pages/loading.dart';
+import 'dart:convert';
+import 'package:lab_11/services/word_time.dart';
+
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at beginning...
+// we can use following code ....
+// initialRoute: '/home',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Loading(),
+          '/home': (context) => Home(),
+        }));
+
+// class Loading extends StatefulWidget {
+//   @override
+//   State<Loading> createState() => _LoadingState();
+// }
+
+// class _LoadingState extends State<Loading> {
+//   String? time = 'LOADING..........';
+//   void setWorldTime() async {
+//     WordTime timeinstance = WordTime(
+//         location: 'Mexico_City',
+//         flag: 'America.png',
+//         url: 'America/Mexico_City');
+//     await timeinstance.getTime();
+// // print(timeinstance.time);
+//     setState(() {
+//       time = timeinstance.time;
+//     });
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     setWorldTime();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Padding(
+//       padding: EdgeInsets.all(80.0),
+//       child: Text(time.toString()),
+//     ));
+//   }
+// }
---
+++ Lab-11/lib/pages/loading.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:lab_11/services/word_time.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setWorldTime() async {
+    WordTime timeinstance = WordTime(
+        location: 'Mexico_City',
+        flag: 'America.png',
+        url: 'America/Mexico_City');
+    await timeinstance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': timeinstance.location,
+      'flag': timeinstance.flag,
+      'time': timeinstance.time,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        body: Center(
+          child: SpinKitFadingCube(
+            color: Colors.white,
+            size: 90.0,
+          ),
+        ));
+  }
+}
